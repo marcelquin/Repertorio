7ae36c5e729ea8bad9f83fd4fdb9c6a278371cf6
Marcel Higor quinteiro Araujo <117388829+marcelquin@users.noreply.github.com>
2023-09-10 15:20:41 -0400

    adição de informações da api
    sem alterações em passos anteriores

--- API/src/main/java/baseAPI/API/ApiApplication.java
+++ API/src/main/java/baseAPI/API/ApiApplication.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(info = @Info(title = "Base APi", version = "1", description = "Api Base"))
+@OpenAPIDefinition(info = @Info(title = "Banda API", version = "1", description = "Gerencia dados de eventos, integrandes e musicas"))
 @SpringBootApplication
 public class ApiApplication {
 
